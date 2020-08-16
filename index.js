a9d968fc42736197e40741a04be705a08dcb2d96
sapphirepencil <djhawro@gmail.com>
2020-08-16 13:41:36 +0100

    2. added message event sent from client, received by server 3. added jQuery on the client

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
