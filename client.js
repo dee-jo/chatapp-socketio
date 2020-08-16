e544d7d7b83680666ef881ccf8a78c397fa56bb2
sapphirepencil <djhawro@gmail.com>
2020-08-16 21:01:23 +0100

    - reorganised client.js, replaced ES5 functions with ES6 arrow notation

--- client.js
+++ client.js
@@ -1,6 +1,7 @@
-$(function () {
-  var socket = io();
-  $('form').submit(function(e) {
+$(() => {
+  const socket = io();
+
+  $('form').submit((e) => {
     e.preventDefault(); // prevents page reloading
     socket.emit('chat message', {
       uId: socket.uId,
@@ -11,27 +12,27 @@ $(function () {
     $('#user').val('');
     return false;
   });
-  socket.on('past messages', function({messages}) {
-    console.log('client received passed messages: ', messages);
-    
-    // var li = messages.length && `<p>User: ${messages[0].user} | Message: ${messages[0].text}</p>`;
-    // $('#past_messages').append($('<li>').html(li));
 
-    messages.forEach(function(message) {
+  socket.on('connected', (msg) => {
+    $('#messages').append($('<li>').text(msg));
+  });
+  
+  socket.on('disconnected', (msg) => {
+    $('#messages').append($('<li>').text(msg))
+  });
+
+  socket.on('past messages', ({messages}) => {
+    console.log('client received passed messages: ', messages);
+    messages.forEach((message) => {
       var li = `<p>User: ${message.user} | Message: ${message.text}</p>`;
       $('#messages').append($('<li>').html(li));
     })
   });
-  socket.on('chat message', function(msg) {
+
+  socket.on('chat message', (msg) => {
     console.log('client received chat message: ',msg)
     var li = `<p>User: ${msg.user} | Message: ${msg.text}</p>`;
     $('#messages').append($('<li>').html(li)); 
   });
-
-  socket.on('connected', function(msg) {
-    $('#messages').append($('<li>').text(msg));
-  });
-  socket.on('disconnected', function(msg) {
-    $('#messages').append($('<li>').text(msg))
-  });
+  
 });
