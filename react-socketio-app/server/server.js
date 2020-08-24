39a62628c8362fed639466a786eecc86dee864f8
sapphirepencil <djhawro@gmail.com>
2020-08-24 15:54:55 +0100

    Modified the server so that each socket receives a dummy list of rooms to join at the start of the connection (this list of rooms would be later filtered per user - i.e. each user will have their own list of rooms they were previously authorised to join. The client successfully receives that list on connection (checked Network tab), now need to integrate it with UI.

--- react-socketio-app/server/server.js
+++ react-socketio-app/server/server.js
@@ -4,9 +4,24 @@ const io = require('socket.io')(http, {
   perMessageDeflate: false
 });
 
+const rooms = ['music', 'sport', 'gardening'];
 
 io.on("connection", socket => {
   console.log("Socket id: ", socket.id, " connected!");
+
+  socket.join(rooms, () => {
+    const rooms = Object.keys(socket.rooms);
+    console.log(rooms); // [ <socket.id>, 'room 237', 'room 238' ]
+
+    io.to(socket.id).emit('joined rooms', rooms);
+
+    rooms.forEach((room, i) => {
+      if (i > 0) {
+        io.to(room).emit('a new user has joined the room', room);
+      }
+    });
+  });
+
 
   socket.on("chat message", message => {
     console.log("Received a message from ", socket.id, ", message: ", message);
@@ -18,6 +33,7 @@ io.on("connection", socket => {
     console.log("Socket id: ", socket.id, " disconnected!");
   });
 });
+
 http.listen(3001, () => {
   console.log('listening on :3001');
 });
